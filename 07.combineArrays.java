fdf67ad272274bbf801d32ed1b76acca429556a4
Tlou Maponya <tlou.maponya@umuzi.org>
2019-11-15 15:11:25 +0200

    combineArrays Updated

---
+++ 07.combineArrays.java
@@ -0,0 +1,41 @@
+package com.example.java;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Please enter the size of your Arrays: ");
+
+        Scanner input = new Scanner(System.in);
+        int amount = input.nextInt();
+
+        System.out.println("Please Enter "+amount+ " Strings for your 1st Array");
+
+        Scanner scanner1 = new Scanner(System.in);
+        String[] firstArray = new String[amount];
+
+        for (int i = 0; i < firstArray.length; i++) {
+            firstArray[i] = scanner1.nextLine();
+        }
+
+        System.out.println("Please Enter "+amount+ " Strings for your 2nd Array");
+
+        Scanner scanner2 = new Scanner(System.in);
+        String[] secondArray = new String[amount];
+
+        for (int i = 0; i < secondArray.length; i++) {
+            secondArray[i] = scanner2.nextLine();
+        }
+
+        //  Output
+        for (int i = 0; i < firstArray.length; i++) {
+            System.out.print(firstArray[i] + " "+ secondArray[i] + " ");
+        }
+
+
+    }
+
+
+}
