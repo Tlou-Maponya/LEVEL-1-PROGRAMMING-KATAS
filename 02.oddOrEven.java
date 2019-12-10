ee60d18425d01332a3a4174da8c25a7d381ad1d0
Tlou <tlou.maponya@umuzi.org>
2019-12-10 10:26:37 +0200

    oddOrEven.java updated

--- 02.oddOrEven.java
+++ 02.oddOrEven.java
@@ -1,22 +1,21 @@
-package com.example.java;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
         System.out.println("Please Enter Number: ");
 
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
 
-        if(number % 2 ==0){
-            System.out.println(number + " is even");
-        }
-        else {
-            System.out.println(number + " is odd");
-        }
+        evenOrOdd(number);
     }
 
-
+    public static void evenOrOdd(int num) {
+        if(num % 2 ==0){
+            System.out.println(num + " is even");
+        }
+        else {
+            System.out.println(num + " is odd");
+        }
+    }
 }
