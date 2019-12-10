9ad5b0b46df08519f9774cc9869e1d802331df27
Tlou <tlou.maponya@umuzi.org>
2019-12-10 10:34:16 +0200

    updated right-handed triangle

--- 04.rightHandedTriangle.java
+++ 04.rightHandedTriangle.java
@@ -1,22 +1,21 @@
-package com.example.java;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Please Enter Unit Size: ");
+        System.out.println("Please Enter Number: ");
 
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
 
-        for (int i = 0; i <= number; i++) {
+        triangle(number);
+    }
+
+    public static void triangle(int num) {
+        for (int i = 0; i <= num; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("#");
             }
             System.out.println("");
         }
     }
-
-
 }
