72fd8311f0b906fc2b99277ec77b3b22e0c52948
Tlou <tlou.maponya@umuzi.org>
2019-12-10 10:38:14 +0200

    Isosceles triangle updated

--- 05.isoscelestriangle.java
+++ 05.isoscelestriangle.java
@@ -1,19 +1,19 @@
-package com.example.java;
-
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int num;
-        System.out.println("Enter number of your choice:");
+        System.out.println("Please Enter Number: ");
+
         Scanner input = new Scanner(System.in);
-        num = input.nextInt();
-        pyramid(num);
+        int number = input.nextInt();
+
+        isosceles(number);
     }
 
-    public static void pyramid(int height) {
-        for (int i = 1; i <= height; i++) {
+    public static void isosceles(int num) {
+        for (int i = 1; i <= num; i++) {
 
-            for (int j = i; j<height; j++)
+            for (int j = i; j<num; j++)
                 System.out.print(" ");
 
             for (int k = 1; k <(i*2); k++)
